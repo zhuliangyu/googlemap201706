5a902741edd9aba10209b465992dd262060fd313
Ricky Zhu <ricky@enbut.com>
2017-06-07 23:13:26 -0700

    first commit

---
+++ js/map.js
@@ -0,0 +1,28 @@
+// /**
+//  * Created by macbook-retina on 2017-06-07.
+//  */
+// var map;
+// function initMap() {
+//     var ululru = {lat: -25, lng: 131};
+//
+//     map = new google.maps.Map(document.getElementById('map'), {
+//         center: ululru,
+//         zoom: 7
+//     });
+//
+//     var marker = new google.maps.Marker({
+//         position: ululru,
+//         map:map
+//     });
+// }
+
+
+//
+// var lines = $('#mytextarea').val().split(/\n/);
+// var texts = [];
+// for (var i=0; i < lines.length; i++) {
+//     // only push this line if it contains a non whitespace character.
+//     if (/\S/.test(lines[i])) {
+//         texts.push($.trim(lines[i]));
+//     }
+// }
